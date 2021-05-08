279a24c2e41ad685c320f6c5d8a98032c645b12d
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-08 05:58:28 +0500

    Added comment field in student model

--- meeting_planner/orm/models.py
+++ meeting_planner/orm/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Student(models.Model):
-    stuid=models.IntegerField()
-    stuname=models.CharField(max_length=70)
-    stuemail=models.EmailField(max_length=70)
-    stuepass=models.CharField(max_length=70)
+    stuid = models.IntegerField()
+    stuname = models.CharField(max_length=70)
+    stuemail = models.EmailField(max_length=70)
+    stuepass = models.CharField(max_length=70)
+    comment = models.CharField(max_length=70, default='not avaiable')
