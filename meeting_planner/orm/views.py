3a815ce99a8317fa014f0e3a54b9820cdda5812f
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-08 06:59:27 +0500

    retreived data from sqllite3

--- meeting_planner/orm/views.py
+++ meeting_planner/orm/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from orm.models import Student
 # Create your views here.
+from django.http import HttpResponse
+
+
+def studentinfo(request):
+    stud = Student.objects.all()
+    return render(request, 'templates/orm/studetails.html', {'stu': stud})
+    # return HttpResponse('egege')
