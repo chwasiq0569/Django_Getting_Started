3635525020694d11b0c4646345d5718bb29a682d
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-09 07:19:05 +0500

    Rendered table using adminmodel

--- meeting_planner/orm/admin.py
+++ meeting_planner/orm/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from orm.models import Student
 
-admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['stuid', 'stuname', 'stuemail', 'stuepass', 'comment']
+
+
+admin.site.register(Student, StudentAdmin)
