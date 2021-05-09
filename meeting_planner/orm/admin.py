181e6af554fd745b3df677c02ef874e98304324f
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-09 07:21:25 +0500

    Rendered Table using decorators

--- meeting_planner/orm/admin.py
+++ meeting_planner/orm/admin.py
@@ -4,8 +4,12 @@ from django.contrib import admin
 from orm.models import Student
 
 
+# class StudentAdmin(admin.ModelAdmin):
+#     list_display = ['stuid', 'stuname', 'stuemail', 'stuepass', 'comment']
+
+
+# admin.site.register(Student, StudentAdmin)
+
+@admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['stuid', 'stuname', 'stuemail', 'stuepass', 'comment']
-
-
-admin.site.register(Student, StudentAdmin)
